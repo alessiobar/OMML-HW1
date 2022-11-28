d3a9ff92a87dd6f7d98d0e9f121568259fb437c9
alessiobar <47196616+alessiobar@users.noreply.github.com>
2022-11-29 00:07:59 +0100

    Add files via upload

---
+++ Question 2/Test_22_OptimizedNoobs.py
@@ -0,0 +1,11 @@
+import pickle
+from functions_22 import *
+from run_22_OptimizedNoobs import *
+def ICanGeneralize(X_test):
+    #X_train,_, Y_train, _ = get_data()
+    #params, train_errors, y, num_grad, num_eval = fit(X_train, Y_train, N = 30, sigma = 1.2, r = 0.01, learning_rate = 0.01, number_of_iterations = 2000, supervised = False)
+    with open('weights22.pkl', 'rb') as f:
+        params = pickle.load(f)
+    y_tst, F, _  = forwardPropagation(X = X_test, params = params)
+    return y_tst
+print(ICanGeneralize(X_train)- Y_train)
