d93cf3d89cd1c57e7f33df519ca5bac72d31adc2
alessiobar <47196616+alessiobar@users.noreply.github.com>
2023-02-22 23:19:22 +0100

    Update test_12.py

--- scripts/Question 1/test_12.py
+++ scripts/Question 1/test_12.py
@@ -1,6 +1,6 @@
 import pickle
 from functions_12 import *
-from run_12_OptimizedNoobs import *
+from run_12 import *
 def ICanGeneralize(X_test):
     #params, train_errors, y, num_grad, num_eval = fit(X_train, Y_train, N = 30, sigma = 1.2, r = 0.01, learning_rate = 0.01, number_of_iterations = 2)
     with open('weights12.pkl', 'rb') as f:
