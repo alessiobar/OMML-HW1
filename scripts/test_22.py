6accba7da2567be19b169c37357c65bb2b2f7d1b
alessiobar <47196616+alessiobar@users.noreply.github.com>
2023-02-22 23:21:06 +0100

    Update and rename Test_22_OptimizedNoobs.py to test_22.py

--- scripts/test_22.py
+++ scripts/test_22.py
@@ -1,6 +1,6 @@
 import pickle
 from functions_22 import *
-from run_22_OptimizedNoobs import *
+from run_22 import *
 def ICanGeneralize(X_test):
     #X_train,_, Y_train, _ = get_data()
     #params, train_errors, y, num_grad, num_eval = fit(X_train, Y_train, N = 30, sigma = 1.2, r = 0.01, learning_rate = 0.01, number_of_iterations = 2000, supervised = False)
